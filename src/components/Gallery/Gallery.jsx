174197d1c153c97e28a36b4f163eb3bd60b87144
archer <458264763@qq.com>
2018-01-23 10:56:21 +0800

    初始化 Gallery 组件

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './gallery.less'
+
+export default class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+    
+    }
+
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
