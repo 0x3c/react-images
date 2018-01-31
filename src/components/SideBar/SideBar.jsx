512648c0129004f367362f164b536e9bd94f5d45
archer <458264763@qq.com>
2018-02-01 03:09:13 +0800

    调整 Nav 激活样式

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,52 +3,48 @@ import PropTypes from 'prop-types'
 
 
 import Hammer from 'react-hammerjs'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './sidebar.less'
 import avatar from '../../common/img/avatar.jpeg'
 
-// 导航组件
-const MyNav=(props)=>(
-        <NavLink to={props.to} className="menu-normal" activeClassName="menu-active">
-            <li>
-                {props.title}
-            </li>
-        </NavLink>
-)
-
-export default class SideBar extends React.Component{
-    constructor(props){
+export default class SideBar extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            sort:{},
-            data:[]
+        this.state = {
+            sort: {},
+            data: []
         };
-        this.callback=this.callback.bind(this);
+        this.callback = this.callback.bind(this);
     }
-    callback(data){
+    callback(data) {
         console.log(data)
     }
-    
-    componentDidMount(){
-        const { handleInitDataSort } =this.props;
+
+    componentDidMount() {
+        const { handleInitDataSort } = this.props;
         // 获取类别
         handleInitDataSort();
     }
-    render(){
-        const {  dataSort, focus, handleHideSidebar } = this.props;
-        
+    render() {
+        const { dataSort, focus, handleHideSidebar } = this.props;
 
-        const nav=((typeof dataSort) ==='object' && dataSort.length>0)
-                ?
-                dataSort.map((item,index)=>(
-                    <MyNav key={index} to={{pathname:item.subUrl, state:item.col}} title={item.col} />
-                ))
-                :
-                null;
 
-        return(
+        const nav = ((typeof dataSort) === 'object' && dataSort.length > 0)
+            ?
+            dataSort.map((item, index) => (
+                // req_col 为请求类别字段,req_title 为请求类别名称
+                <NavLink key={index} to={{ pathname: item.subUrl, state: {req_col:item.col,req_title:item.col} }}
+                 className="menu-normal" activeStyle={{"backgroundColor": "lightblue"}}
+                 onClick={handleHideSidebar} >
+                    {item.col}
+                </NavLink>
+            ))
+            :
+            null;
+
+        return (
             <Hammer onSwipeLeft={handleHideSidebar} >
-                <div className={focus ? "sidebar-container":"sidebar-container sidebar-container-blur"}>
+                <div className={focus ? "sidebar-container" : "sidebar-container sidebar-container-blur"}>
                     {/* 头像区 */}
                     <div className="sidebar-user">
                         <div className="user-warpper">
@@ -61,14 +57,14 @@ export default class SideBar extends React.Component{
                     {/* 菜单区 */}
                     <ul className="sidebar-menu">
                         {/* 导航连接 */}
-                        { nav }
+                        {nav}
                     </ul>
                 </div>
             </Hammer>
         )
     }
 }
-SideBar.propTypes={
-    focus:PropTypes.bool.isRequired,
-    handleHideSidebar:PropTypes.func.isRequired,
+SideBar.propTypes = {
+    focus: PropTypes.bool.isRequired,
+    handleHideSidebar: PropTypes.func.isRequired,
 }
